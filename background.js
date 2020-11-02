277b29277793dff70732fe398723d80e911827b6
Sidharth <sidnagar08@gmail.com>
2020-11-02 16:47:13 +0530

    Initial Commit

---
+++ background.js
@@ -0,0 +1,15 @@
+/*
+    Sagar
+    B.voc
+    github: https://github.com/sagar-001
+*/
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === "download") {
+        for(let i = 0; i < request.data.length; i++){
+            chrome.downloads.download({"url": request.data[i].src});
+        }
+
+        sendResponse("Done")
+    }
+})
---
+++ content.js
@@ -0,0 +1,25 @@
+/*
+    Sagar
+    B.voc
+    github: https://github.com/sagar-001
+*/
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === "get_images") {
+        let formatted_images = [];
+
+        let images = $('img').map((i, img) => {
+            if($(img).data('src')) {
+                formatted_images.push({
+                    src: $(img).data('src')
+                })
+            } else if($(img).src) {
+                formatted_images.push({
+                    src: $(img).src
+                })
+            }
+        })
+
+        sendResponse(formatted_images);
+    }
+})
